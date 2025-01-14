3c4cf7e505de03e667fd14a96960b3f6667c4529
Taeksin Kim <90402009+taeksin@users.noreply.github.com>
2025-01-14 17:59:21 +0900

    add embedding code

--- src/Faiss_create_embeddings.py
+++ src/Faiss_create_embeddings.py
@@ -14,9 +14,7 @@ os.environ["OPENAI_API_KEY"] = api_key
 
 # 엑셀 파일 목록 및 시트 목록
 excel_files = [
-    ("data/데이터전처리_서울시복지재단.xlsx", ["기타 출처(47~51)"]),
-    ("data/데이터전처리_한국과학기술기획평가원.xlsx", ["메뉴얼", "규정집"]),
-    ("data/데이터전처리_법무부.xlsx", ["신상정보", "행정지원"]),
+    ("data_source/세무사데이터.xlsx", ["Sheet1"]),
 ]
 
 # 모든 데이터를 하나의 리스트에 저장
@@ -30,13 +28,23 @@ for file, sheets in excel_files:
         
         # 각 행을 Document로 변환하여 리스트에 추가
         for index, row in df.iterrows():
-            content = "\n".join([f"{col}: {row[col]}" for col in df.columns])
-            documents.append(Document(page_content=content, metadata={"source": f"{file} - {sheet}"}))
+            # content는 제목과 본문_원본을 포함
+            content = f"제목: {row['제목']}\n본문: {row['본문_원본']}"
+            
+            # metadata는 파일명과 문서명을 포함
+            metadata = {
+                "파일명": row["파일명"],
+                "문서명": row["문서명"],
+                "source": f"{file} - {sheet}"
+            }
+            
+            documents.append(Document(page_content=content, metadata=metadata))
+print(f"documents: {documents}")
 
-# 임베딩 생성
-embedding_model = OpenAIEmbeddings()
-vectorstore = FAISS.from_documents(documents=documents, embedding=embedding_model)
+# # 임베딩 생성
+# embedding_model = OpenAIEmbeddings()
+# vectorstore = FAISS.from_documents(documents=documents, embedding=embedding_model)
 
-# 벡터 저장소와 메타데이터를 파일로 저장
-vectorstore.save_local("faiss_index")
-print("모든 시트의 임베딩이 성공적으로 저장되었습니다.")
+# # 벡터 저장소와 메타데이터를 파일로 저장
+# vectorstore.save_local("faiss_index")
+# print("모든 시트의 임베딩이 성공적으로 저장되었습니다.")
